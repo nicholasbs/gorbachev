e20b7675cdcf9425951031ba9f20c69bac72e73d
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2011-07-28 14:54:56 -0400

    First pass at bypassing the Times paywall.

---
+++ gorbachev.js
@@ -0,0 +1,9 @@
+var loc = window.location
+if (/.*\?.*$/.test(loc.href)) { // Is there a query string?
+  var url = loc.protocol + "//" + loc.hostname + loc.pathname; // remove it
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, false); // synchronous request 
+  xhr.send();
+  var encodedDoc = encodeURIComponent(xhr.response);
+  location.replace("data:text/html," + encodedDoc);
+}
