474882c2325377f15a54adc3f3de314f585ba028
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2011-07-30 16:18:26 -0400

    Only show single-page articles

--- gorbachev.js
+++ gorbachev.js
@@ -1,6 +1,6 @@
 var loc = window.location
 if (/.*\?.*$/.test(loc.href)) { // Is there a query string?
-  var url = loc.protocol + "//" + loc.hostname + loc.pathname; // remove it
+  var url = loc.protocol + "//" + loc.hostname + loc.pathname + "?pagewanted=all";
   var xhr = new XMLHttpRequest();
   xhr.open("GET", url, false); // synchronous request 
   xhr.send();
